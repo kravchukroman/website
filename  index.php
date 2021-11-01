0d07c2ba69e9a6e16dbc0d1531681f38d53f5f58
kravchukroman <34639794+kravchukroman@users.noreply.github.com>
2021-11-01 10:38:34 +0200

    Create index.php

---
+++  index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Тестова вебсторінка</title>
+</head>
+<body>
+	<?php
+        <h1>Ласкавио просимо на нашу веб-сторінку</h1>
+	?>
+</body>
+</html>
